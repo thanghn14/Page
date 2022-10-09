d86b7e956c664d2792bac0ffafa81cd8ba85caa9
thanghn <thanghongocql@gmail.com>
2022-10-09 17:46:06 +0700

    Update

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -1,1 +1,25 @@
-const clickBuy = document.querySelectorAll('.js-buy')
+
+const buyBtns = document.querySelectorAll('.js-buy')
+const payMain = document.querySelector('.js-pay')
+const closeBtn = document.querySelector('.js-close')
+const payForm = document.querySelector('.js-pay-form')
+
+const showForm = () => {
+    payMain.classList.add('open')
+}
+
+const closeForm = () => {
+    payMain.classList.remove('open')
+}
+
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showForm)
+}
+
+closeBtn.addEventListener('click', closeForm)
+
+payMain.addEventListener('click', closeForm)
+
+payForm.addEventListener('click', (event) => {
+    event.stopPropagation()
+})
